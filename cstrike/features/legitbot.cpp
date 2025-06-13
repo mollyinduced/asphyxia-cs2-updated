9573ee38d93ce6289a830980c9c0b5cd86e7c476
mollyinduced <60119170+mollyinduced@users.noreply.github.com>
2025-06-13 09:57:14 -0400

    removal

--- cstrike/features/legitbot.cpp
+++
@@ -1,9 +0,0 @@
-#include "legitbot.h"
-
-// used: movement callback
-#include "legitbot/aim.h"
-
-void F::LEGITBOT::OnMove(CUserCmd* pCmd, CBaseUserCmdPB* pBaseCmd, CCSPlayerController* pLocalController, C_CSPlayerPawn* pLocalPawn)
-{
-	AIM::OnMove(pCmd, pBaseCmd, pLocalController, pLocalPawn);
-}
